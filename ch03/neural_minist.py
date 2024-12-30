3c04c9c530243c8bdd36c3c50a24ccfc3586e4e6
moons14 <moons@moons14.com>
2024-12-31 02:47:38 +0900

    show image

--- ch03/neural_minist.py
+++ ch03/neural_minist.py
@@ -3,11 +3,24 @@ import sys
 
 sys.path.append(os.pardir)
 
+import numpy as np
+from PIL import Image
+
 from dataset.mnist import load_mnist
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+img = x_train[0]
+label = t_train[0]
+print(label)
+
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
