6e00fa69ee65c2aabb390aea54b32a2b331391e6
moons14 <moons@moons14.com>
2024-12-31 03:05:05 +0900

    run neural minist

--- ch03/neural_minist.py
+++ ch03/neural_minist.py
@@ -3,24 +3,53 @@ import sys
 
 sys.path.append(os.pardir)
 
+import pickle
+
 import numpy as np
-from PIL import Image
+from sigmoid_function import sigmoid_function
+from softmax_function import softmax_function
 
 from dataset.mnist import load_mnist
 
 
-def img_show(img):
-    pil_img = Image.fromarray(np.uint8(img))
-    pil_img.show()
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(
+        normalize=True, flatten=True, one_hot_label=False
+    )
+    return x_test, t_test
 
 
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
-img = x_train[0]
-label = t_train[0]
-print(label)
+def init_network():
+    with open("sample_weight.pkl", "rb") as f:
+        network = pickle.load(f)
 
-print(img.shape)
-img = img.reshape(28, 28)
-print(img.shape)
+    return network
 
-img_show(img)
+
+def predict(network, x):
+    W1, W2, W3 = network["W1"], network["W2"], network["W3"]
+    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
+
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid_function(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid_function(a2)
+    a3 = np.dot(z2, W3) + b3
+    y = softmax_function(a3)
+
+    return y
+
+
+x, t = get_data()
+network = init_network()
+
+batch_size = 100
+accuracy_cnt = 0
+
+for i in range(0, len(x), batch_size):
+    x_batch = x[i : i + batch_size]
+    y_batch = predict(network, x_batch)
+    p = np.argmax(y_batch, axis=1)
+    accuracy_cnt += np.sum(p == t[i : i + batch_size])
+
+print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
