01c4af762889fd5ab92107c5c43ad0f0d15afb53
moons14 <moons@moons14.com>
2025-01-01 15:03:14 +0900

    batch cross_entropy_error

--- functions/cross_entropy_error.py
+++ functions/cross_entropy_error.py
@@ -2,5 +2,10 @@ import numpy as np
 
 
 def cross_entropy_error(y, t):
+    if y.nidm == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
     delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
