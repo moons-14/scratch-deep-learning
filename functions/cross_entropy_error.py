c9e510dad78f3a580aed18519d943f32383c7042
moons14 <moons@moons14.com>
2025-01-01 16:07:20 +0900

    fix: functions

--- functions/cross_entropy_error.py
+++ functions/cross_entropy_error.py
@@ -6,6 +6,9 @@ def cross_entropy_error(y, t):
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
 
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
     batch_size = y.shape[0]
     delta = 1e-7
     return -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
--- functions/numerical_gradient.py
+++ functions/numerical_gradient.py
@@ -1,21 +1,22 @@
 import numpy as np
 
 
-def numerical_gradient(f, x):
+def numerical_gradient(f, X):
     h = 1e-4
-    grad = np.zeros_like(x)
+    grad = np.zeros_like(X)
 
-    for idx in range(x.size):
-        tmp_val = x[idx]
-        # f(x + h) の計算
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
+    it = np.nditer(X, flags=["multi_index"], op_flags=[["readwrite"]])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = X[idx]
+        X[idx] = float(tmp_val) + h
+        fxh1 = f(X)  # f(x + h)
 
-        # f(x - h) の計算
-        x[idx] = tmp_val - h
-        fxh2 = f(x)
-
+        X[idx] = tmp_val - h
+        fxh2 = f(X)  # f(x - h)
         grad[idx] = (fxh1 - fxh2) / (2 * h)
-        x[idx] = tmp_val
+
+        X[idx] = tmp_val  # 値を元に戻す
+        it.iternext()
 
     return grad
